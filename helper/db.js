9778d06b8e02b55a7de2b1a5a24b1f636752a671
Enderhan Kocazeybek <ekzeybek@gmail.com>
2018-02-24 17:50:39 +0300

    1.0

--- helper/db.js
+++ helper/db.js
@@ -1,1 +1,7 @@
 const mongoose=require("mongoose");
+module.exports=function() {  
+    mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds249128.mlab.com:49128/movie");
+    mongoose.connection.on('open',function(){
+        console.log("mongo bağlandı");
+    })
+}
